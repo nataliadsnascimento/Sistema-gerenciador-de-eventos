52f3342ed53212210a0c8df2bd2d3ff825b71f9e
Jamili Martins de Oliveira <milimartins133@gmail.com>
2026-05-30 18:37:36 -0300

    criação da tabela de atividades

---
+++ Scripts/Criação/Atividades.sql
@@ -0,0 +1,13 @@
+CREATE TABLE IF NOT EXISTS atividade (
+    id_atividade SERIAL PRIMARY KEY,
+    nome_atividade VARCHAR(150) NOT NULL, 
+    descricao TEXT,                       
+    data DATE NOT NULL,
+    hora_inicio TIME NOT NULL,
+    hora_fim TIME NOT NULL,
+    local VARCHAR(100),
+    capacidade INT NOT NULL,
+    carga_horaria INT NOT NULL,
+    id_evento INT NOT NULL,
+    FOREIGN KEY (id_evento) REFERENCES evento(id_evento) ON DELETE CASCADE
+);

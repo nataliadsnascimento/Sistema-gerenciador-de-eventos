c4bcdd1edd1d6f4514ba70d94bdffb96ef810a1c
Natália Nascimento <nataliasanntos161@gmail.com>
2026-06-04 10:56:00 -0300

    Popula a tabela organizador

---
+++ Scripts/População/Organizador.sql
@@ -0,0 +1,3 @@
+INSERT INTO organizador (id_usuario, departamento, cargo, curso) VALUES
+(1, 'Departamento de Informática', 'Coordenador de Eventos Extensão', 'Sistemas de Informação'),
+(2, 'Pró-Reitoria de Acadêmica', 'Assistente Administrativo', 'Administração');

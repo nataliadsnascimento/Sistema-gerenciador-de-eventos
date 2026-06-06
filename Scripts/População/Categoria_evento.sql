0ddaef004968cef0dac73660a9a7f0b3f8a0c1a0
lucas-allb <albuquerque.trabalho5625@gmail.com>
2026-06-06 18:19:28 -0300

    povoamento da tabela evento_organizado

---
+++ Scripts/População/Categoria_evento.sql
@@ -0,0 +1,7 @@
+insert into categoria_evento(nome_categoria, descricao, cor) 
+values
+('Tecnologia e Inovação', 'Conferencia sobre o desenvolvimento de software, Ia e Infraestrutura', '#002244'),
+('Ciencia e Academia', 'Semanas universitarias, simpósios de pesquisa e extensão', '#008080'),
+('Negocios e Empreendedorismo', 'Workshops de gestão, startups e networking profissional', '#FF5733'),
+('Artes e Cultura', 'Exposição, mostras culturais e eventos de design dentro da instituição', '#8E44AD'),
+('Saúde e Bem-Estar', 'Palestras sobre a saúde mental na universidade e ergonomia', '#2ECC71');
---
+++ Scripts/População/Evento_organizado.sql
@@ -0,0 +1,7 @@
+insert into evento_organizado(funcao, id_usuario, id_evento)
+values
+('Coordenador Geral', 1, 1),
+('Apoio de Logística', 2, 1),
+('Coordenador Geral', 2, 2),
+('Presidente da Banca', 1, 2),
+('Coordenador Operacional', 1, 3);

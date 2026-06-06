ad3edf0cd6751a36945585e7f7c4a255e915d176
lucas-allb <albuquerque.trabalho5625@gmail.com>
2026-06-06 18:23:53 -0300

    povoamento da tabela auditorio

---
+++ Scripts/População/Auditorio.sql
@@ -0,0 +1,4 @@
+insert into auditorio(possui_palco, quantidade_assento, acessibilidade, id_local)
+values
+(true, 250, 'Rampa de acesso lateral, espaço reservado para cadeirantes, intérprete de LIBRAS', 1),
+(false, 100, 'Elevador de acesso ao andar, assentos preferenciais', 3);

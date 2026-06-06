0d59e72daf4414b868762e929078bdce9fe8ace0
lucas-allb <albuquerque.trabalho5625@gmail.com>
2026-06-06 18:20:21 -0300

    povoamento da tabela evento_patrocinador

---
+++ Scripts/População/Evento_patrocinador.sql
@@ -0,0 +1,8 @@
+insert into evento_patrocinador(tipo_cota, valor_cota, id_evento, id_patrocinador)
+values
+('Cota Diamante', 15000, 1, 1),
+('Cota Ouro', 8000, 1, 2),
+('Cota Bronze', 2000, 1, 4),
+('Cota Prata', 5000, 2, 3),
+('Cota Ouro', 10000, 3, 2);
+

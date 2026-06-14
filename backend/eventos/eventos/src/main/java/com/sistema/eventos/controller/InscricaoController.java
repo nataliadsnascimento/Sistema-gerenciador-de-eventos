a528b8f9e194cc5cd4164a43fe934cf60c1c88cf
Natália Nascimento <nataliasanntos161@gmail.com>
2026-06-14 10:51:08 -0300

    feat: cria os endpoints de pagamento e cancelar inscricão

--- backend/eventos/eventos/src/main/java/com/sistema/eventos/controller/InscricaoController.java
+++ backend/eventos/eventos/src/main/java/com/sistema/eventos/controller/InscricaoController.java
@@ -1,5 +1,6 @@
 package com.sistema.eventos.controller;
 
+import com.sistema.eventos.dto.ConfirmacaPagamentoDTO;
 import com.sistema.eventos.dto.RegistroInscricaoDTO;
 import com.sistema.eventos.repository.InscricaoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -28,6 +29,36 @@ public class InscricaoController {
             return ResponseEntity.ok("Participante registado e inscrição pendente criada com sucesso!");
         } catch (Exception e) {
             return ResponseEntity.badRequest().body("Erro ao realizar inscrição: " + e.getMessage());
+        }
+    }
+
+    @PutMapping("/pagamento")
+    public ResponseEntity<String> confirmarPagamento(ConfirmacaPagamentoDTO dto) {
+        try {
+            inscricaoRepository.confirmarPagamento(
+                    dto.getIdInscricao(),
+                    dto.getEhPago(),
+                    dto.getValor(),
+                    dto.getOpcaoPagamento(),
+                    dto.getDataVencimento(),
+                    dto.getComprovante(),
+                    dto.getCotaSocial(),
+                    dto.getMotivoIsencao(),
+                    dto.getComprovanteIsencao()
+            );
+            return ResponseEntity.ok("Pagamento processado e status da inscrição atualizado!");
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().body("Erro ao processar pagamento: " + e.getMessage());
+        }
+    }
+
+    @DeleteMapping("/cancelar/{id}")
+    public ResponseEntity<String> cancelarInscricao(@PathVariable("id") Integer id) {
+        try {
+            inscricaoRepository.cancelarInscricao(id);
+            return ResponseEntity.ok("Inscrição cancelada com sucesso. Vaga liberada!");
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().body("Erro ao cancelar inscrição: " + e.getMessage());
         }
     }
 }
